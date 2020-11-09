368065907ab923d92a19bc5918b53314d538ea64
MrJale <37137503+MrJale@users.noreply.github.com>
2020-11-08 22:41:21 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-./start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /CISC5950P1Q2.1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /CISC5950P1Q2.1/shot_logs.csv /CISC5950P1Q2.1/input/
+bash start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /BigData_Q2_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /BigData_Q2_1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /BigData_Q2_1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /BigData_Q2_1/shot_logs.csv /BigData_Q2_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file /CISC5950P1Q2.1/mapper.py -mapper /CISC5950P1Q2.1/mapper.py \
--file /CISC5950P1Q2.1/reducer.py -reducer /CISC5950P1Q2.1/reducer.py \
--input /project1.1/input/* -output /CISC5950P1Q2.1/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /CISC5950P1Q2.1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /CISC5950P1Q2.1/output/
-./stop.sh
+-file /BigData_Q2_1/mapper.py -mapper /BigData_Q2_1/mapper.py \
+-file /BigData_Q2_1/reducer.py -reducer /BigData_Q2_1/reducer.py \
+-input /BigData_Q2_1/input/* -output /BigData_Q2_1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /BigData_Q2_1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /BigData_Q2_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /BigData_Q2_1/output/
+bash stop.sh
